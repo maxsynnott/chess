2749e53e0c4beca9d6bd2315befb9ca5e7392b8a
maxsynnott <maxryansynnott@gmail.com>
2020-07-13 11:54:22 +0200

    show route

--- api/v1/matches.js
+++ api/v1/matches.js
@@ -5,11 +5,30 @@ const router = express.Router();
 const knex = require('../../db/knex');
 
 router.get('/', (req, res) => {
-	const query = knex('matches');
+	const query = knex
+		.select('*')
+		.from('matches');
 
 	query.then((matches) => {
 		res.json(matches);
 	});
+});
+
+router.get('/:id', (req, res, next) => {
+	const query = knex
+		.select('*')
+		.from('matches')
+		.where('id', req.params.id)
+		.limit(1)
+		.first()
+
+	query.then((match) => {
+		if (match) {
+			res.json(match);
+		} else {
+			next();
+		}
+	})
 });
 
 module.exports = router;
