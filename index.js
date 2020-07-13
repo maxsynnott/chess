13a08ac989d0926e77b9156896305bca59a8b847
maxsynnott <maxryansynnott@gmail.com>
2020-07-13 11:24:02 +0200

    beginning of api routing + middleware

--- index.js
+++ index.js
@@ -1,18 +1,28 @@
-const express = require('express')
+const express = require('express');
 
-const app = express()
+const morgan = require('morgan');
+const cookieParser = require('cookie-parser');
+const bodyParser = require('body-parser');
 
-app.get('/', (req, res) => {
-	res.send('Hello World!')
-})
+const matches = require('./api/v1/matches');
 
+const app = express();
 
+app.use(morgan('dev'));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser());
+
+app.use('/api/v1/matches', matches);
+
+// Catch 404 and forward to error handler
 app.use((req, res, next) => {
 	const err = new Error('Not Found');
 	err.status = 404;
 	next(err)
 })
 
+// Error handler
 app.use((err, req, res, next) => {
 	res.status(err.status || 500);
 	res.json({
@@ -20,7 +30,6 @@ app.use((err, req, res, next) => {
 		error: req.app.get('env') === 'development' ? err : {}
 	})
 })
-
 
 const port = process.env.PORT || 3000
 
